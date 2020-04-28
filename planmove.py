e275f73f90d5b613f360d2a3cf836b9a59fc1ed8
Iaroslav Sokolov <sokolov.yas@gmail.com>
2020-04-28 17:17:33 +0300

    Reformat code

--- planmove.py
+++ planmove.py
@@ -1,9 +1,12 @@
 # Made according to this (many thanks):
 # https://www.youtube.com/watch?v=Mdg9ElewwA0&feature=emb_logo
 
-import pygame, math, time, random
+import math
+import random
+import time
+
 import numpy as np
-from pygame.locals import *
+import pygame
 
 pygame.init()
 
@@ -25,9 +28,9 @@ BARRIERRADIUS = 0.14
 ROBOTRADIUS = 0.14
 WHEELBLOB = 0.04
 ROBOTWIDTH = 2 * ROBOTRADIUS
-MAXVELOCITY = 0.3      # ms^(-1) max speed of each wheel
+MAXVELOCITY = 0.3  # ms^(-1) max speed of each wheel
 BARRIERVELOCITYRANGE = 0.15
-PLAYFIELDCORNERS = (-4.0, -2.5, 4.0, 2.5) # The region we will fill with obstacles
+PLAYFIELDCORNERS = (-4.0, -2.5, 4.0, 2.5)  # The region we will fill with obstacles
 
 # Starting pose of robot
 x = -4.0
@@ -50,31 +53,37 @@ barriers = []
 
 # Generate 9 random barriers
 for i in range(9):
-    (bx, by, vx, vy) = (random.uniform(PLAYFIELDCORNERS[0], PLAYFIELDCORNERS[2]),
-                        random.uniform(PLAYFIELDCORNERS[1], PLAYFIELDCORNERS[3]),
-                        random.gauss(0.0, BARRIERVELOCITYRANGE),
-                        random.gauss(0.0, BARRIERVELOCITYRANGE))
+    (bx, by, vx, vy) = (
+        random.uniform(PLAYFIELDCORNERS[0], PLAYFIELDCORNERS[2]),
+        random.uniform(PLAYFIELDCORNERS[1], PLAYFIELDCORNERS[3]),
+        random.gauss(0.0, BARRIERVELOCITYRANGE),
+        random.gauss(0.0, BARRIERVELOCITYRANGE),
+    )
     barrier = [bx, by, vx, vy]
     barriers.append(barrier)
 
 # Ball will be just another barrier which doesn't move
-(bx, by, vx, vy) = (PLAYFIELDCORNERS[2],
-                    PLAYFIELDCORNERS[3],
-                    random.gauss(0.0, BARRIERVELOCITYRANGE),
-                    random.gauss(0.0, BARRIERVELOCITYRANGE))
+(bx, by, vx, vy) = (
+    PLAYFIELDCORNERS[2],
+    PLAYFIELDCORNERS[3],
+    random.gauss(0.0, BARRIERVELOCITYRANGE),
+    random.gauss(0.0, BARRIERVELOCITYRANGE),
+)
 barrier = [bx, by, vx, vy]
 barriers.append(barrier)
 targetindex = 9
 
+
 # For debugging, prints locations of all barriers (other players and ball)
-def printBarriers():
-    for (i, barrier) in enumerate(barriers):
+def print_barriers():
+    for i, barrier in enumerate(barriers):
         print(i, barrier[0], barrier[1], barrier[2], barrier[3])
 
+
 # Moves other players (not ball)
-def moveBarriers(dt):
-    for (i, barrier) in enumerate(barriers):
-        if i != targetindex: # we don't want to move the ball
+def move_barriers(dt):
+    for i, barrier in enumerate(barriers):
+        if i != targetindex:  # we don't want to move the ball
             barriers[i][0] += barriers[i][2] * dt
             if barriers[i][0] < PLAYFIELDCORNERS[0]:
                 barriers[i][2] = -barriers[i][2]
@@ -85,6 +94,7 @@ def moveBarriers(dt):
                 barriers[i][3] = -barriers[i][3]
             if barriers[i][1] > PLAYFIELDCORNERS[3]:
                 barriers[i][3] = -barriers[i][3]
+
 
 # Constants for graphics display
 # Transformation from metric world frame to graphics frame
@@ -105,7 +115,7 @@ white = (255, 255, 255)
 blue = (0, 0, 255)
 grey = (70, 70, 70)
 green = (0, 204, 0)
-k = 100 # pixels per metre for graphics
+k = 100  # pixels per metre for graphics
 
 # Screen centre will correspond to (x, y) = (0, 0)
 u0 = WIDTH / 2
@@ -117,12 +127,13 @@ screen = pygame.display.set_mode(size)
 # Array for path choices use for graphics
 pathstodraw = []
 
+
 # Set new robot position in simulation
 # based on current pose and velocity controls.
 #
 # Uses time deltat in future.
 # Returns xnew, ynew, thetanew. Also returns path which is just used for graphics.
-def setNewPosition(vL, vR, x, y, theta, deltat):
+def set_new_position(vL, vR, x, y, theta, deltat):
     # Simple special cases
     # Straight line motion
     if round(vL, 3) == round(vR, 3):
@@ -145,81 +156,83 @@ def setNewPosition(vL, vR, x, y, theta, deltat):
         ynew = y - R * (math.cos(deltatheta + theta) - math.cos(theta))
         thetanew = theta + deltatheta
 
-    return (xnew, ynew, thetanew)
+    return xnew, ynew, thetanew
+
 
 # Draw the barriers (other players and ball) on the screen
-def drawBarriers(barriers):
-    for (i, barrier) in enumerate(barriers):
+def draw_barriers(barriers):
+    for i, barrier in enumerate(barriers):
         if i == targetindex:
             bcol = red
         else:
             bcol = lightblue
-        pygame.draw.circle(screen, bcol,
-                           (int(u0 + k * barrier[0]), int(v0 - k * barrier[1])),
-                           int(k * BARRIERRADIUS), 0)
+        pygame.draw.circle(
+            screen, bcol, (int(u0 + k * barrier[0]), int(v0 - k * barrier[1])), int(k * BARRIERRADIUS), 0
+        )
+
 
 # Calculate the closest obstacle at a position (x, y)
-def calculateClosestObstacleDistance(x, y):
+def calculate_closest_obstacle_distance(x, y):
     closestdist = 100000.0
     # Calculate distance to closest obstacle
-    for (i, barrier) in enumerate(barriers):
+    for i, barrier in enumerate(barriers):
         if i != targetindex:
             dx = barrier[0] - x
             dy = barrier[1] - y
-            d = math.sqrt(dx**2 + dy**2)
+            d = math.sqrt(dx ** 2 + dy ** 2)
             # Distance between closest touching point of circular robot and circular barrier
             dist = d - BARRIERRADIUS - ROBOTRADIUS
             if dist < closestdist:
                 closestdist = dist
     return closestdist
 
-def calculateKsiVector(v, omega):
-    matrix_3 = np.array([[math.cos(theta), 0],
-                         [math.sin(theta), 0],
-                         [0, 1]])
 
-    matrix_4 = np.array([[v],
-                         [omega]])
+def calculate_xi_vector(v, omega):
+    matrix_3 = np.array([[math.cos(theta), 0], [math.sin(theta), 0], [0, 1]])
+
+    matrix_4 = np.array([[v], [omega]])
 
     ksi = np.dot(matrix_3, matrix_4)
     return ksi
 
-def calculatePhiVector(ksi):
-    matrix_1 = np.array([[1, 0, l],
-                         [1, 0, -l],
-                         [0, 1, 0]])
 
-    matrix_2 = np.array([[math.cos(theta), math.sin(theta), 0],
-                         [-math.sin(theta), math.cos(theta), 0],
-                         [0, 0, 1]])
+def calculate_phi_vector(ksi):
+    matrix_1 = np.array([[1, 0, l], [1, 0, -l], [0, 1, 0]])
 
-    res = 1/r * np.dot(matrix_1, matrix_2)
+    matrix_2 = np.array([[math.cos(theta), math.sin(theta), 0], [-math.sin(theta), math.cos(theta), 0], [0, 0, 1]])
+
+    res = 1 / r * np.dot(matrix_1, matrix_2)
     phi = np.dot(res, ksi)
 
-    return (phi[0][0], phi[1][0])
+    return phi[0][0], phi[1][0]
 
 
-def moveToDot(target_x, target_y):
+def move_to_dot(target_x, target_y):
     # x and y here are robot coordinates
-    dx = (target_x - x)
-    dy = (target_y - y)
+    dx = target_x - x
+    dy = target_y - y
 
-    ro_new = math.sqrt(dx**2 + dy**2)
+    ro_new = math.sqrt(dx ** 2 + dy ** 2)
     alpha_new = -theta + math.atan2(dy, dx)
     beta_new = -theta - alpha_new
 
     v = k_ro * ro_new
     omega = k_alpha * alpha_new + k_beta * beta_new
 
-    ksi = calculateKsiVector(v, omega)
-    (vLchosen, vRchosen) = calculatePhiVector(ksi)
+    ksi = calculate_xi_vector(v, omega)
+    vLchosen, vRchosen = calculate_phi_vector(ksi)
 
-    return (vLchosen, vRchosen, ro_new, alpha_new, beta_new)
+    return vLchosen, vRchosen, ro_new, alpha_new, beta_new
 
-def moveToDotAgain(target_x, target_y):
-    change_rate = np.array([[-k_ro * ro * math.cos(alpha)],
-                            [k_ro * math.sin(alpha) - k_alpha * alpha - k_beta * beta],
-                            [-k_ro * math.sin(alpha)]])
+
+def move_to_dot_again(target_x, target_y):
+    change_rate = np.array(
+        [
+            [-k_ro * ro * math.cos(alpha)],
+            [k_ro * math.sin(alpha) - k_alpha * alpha - k_beta * beta],
+            [-k_ro * math.sin(alpha)],
+        ]
+    )
 
     ro_new = ro + change_rate[0][0] * dt
     alpha_new = alpha + change_rate[1][0] * dt
@@ -228,19 +241,21 @@ def moveToDotAgain(target_x, target_y):
     v = k_ro * ro_new
     omega = k_alpha * alpha_new + k_beta * beta_new
 
-    ksi = calculateKsiVector(v, omega)
-    (vLchosen, vRchosen) = calculatePhiVector(ksi)
+    ksi = calculate_xi_vector(v, omega)
+    vLchosen, vRchosen = calculate_phi_vector(ksi)
 
-    return (vLchosen, vRchosen, ro_new, alpha_new, beta_new)
+    return vLchosen, vRchosen, ro_new, alpha_new, beta_new
 
-def obstacleAvoidance():
+
+def obstacle_avoidance():
     # No obstacle avoidance by default, just move to the ball
     target_x = x + 0.5
     target_y = y + 0.35
     # target_x = PLAYFIELDCORNERS[2]
     # target_y = PLAYFIELDCORNERS[3]
 
-    return (target_x, target_y)
+    return target_x, target_y
+
 
 # We will calculate time
 startTime = time.time()
@@ -256,17 +271,17 @@ while True:
     locationhistory.append((x, y))
 
     # Planning
-    disttotarget = math.sqrt((x - target_x)**2 + (y - target_y)**2)
+    disttotarget = math.sqrt((x - target_x) ** 2 + (y - target_y) ** 2)
     if disttotarget < (ROBOTRADIUS + 0.3):
         # print("Calling Obstacle Avoidance algorithm")
         # Calculate best target point and call moveToDot
 
         # Identify ball nad players positions
         screen_picture = pygame.surfarray.array2d(screen)
-        # (ball_predicted_positions, barriers_predicted_positions) = getBarriersPositions(screen_picture, ((red, 1), (lightblue, 9)))
+        # ball_predicted_positions, barriers_predicted_positions = get_barriers_positions(screen_picture, ((red, 1), (lightblue, 9)))
 
-        (target_x, target_y) = obstacleAvoidance()
-        (vL, vR, ro, alpha, beta) = moveToDot(target_x, target_y)
+        target_x, target_y = obstacle_avoidance()
+        vL, vR, ro, alpha, beta = move_to_dot(target_x, target_y)
 
         # if vL > MAXVELOCITY or vR > MAXVELOCITY:
         #     if vL > vR:
@@ -282,7 +297,7 @@ while True:
         #         vR = 0.3
     else:
         # print("stillMovingToDot")
-        (vL, vR, ro, alpha, beta) = moveToDotAgain(target_x, target_y)
+        vL, vR, ro, alpha, beta = move_to_dot_again(target_x, target_y)
 
         # if vL > MAXVELOCITY or vR > MAXVELOCITY:
         #     if vL > vR:
@@ -300,7 +315,7 @@ while True:
     screen.fill(black)
     for loc in locationhistory:
         pygame.draw.circle(screen, grey, (int(u0 + k * loc[0]), int(v0 - k * loc[1])), 3, 0)
-    drawBarriers(barriers)
+    draw_barriers(barriers)
 
     # Draw robot
     u = u0 + k * x
@@ -308,14 +323,14 @@ while True:
     pygame.draw.circle(screen, white, (int(u), int(v)), int(k * ROBOTRADIUS), 3)
     # Draw wheels as little blobs so you can see robot orientation
     # left wheel centre
-    wlx = x - (ROBOTWIDTH/2.0) * math.sin(theta)
-    wly = y + (ROBOTWIDTH/2.0) * math.cos(theta)
+    wlx = x - (ROBOTWIDTH / 2.0) * math.sin(theta)
+    wly = y + (ROBOTWIDTH / 2.0) * math.cos(theta)
     ulx = u0 + k * wlx
     vlx = v0 - k * wly
     pygame.draw.circle(screen, blue, (int(ulx), int(vlx)), int(k * WHEELBLOB))
     # right wheel centre
-    wrx = x + (ROBOTWIDTH/2.0) * math.sin(theta)
-    wry = y - (ROBOTWIDTH/2.0) * math.cos(theta)
+    wrx = x + (ROBOTWIDTH / 2.0) * math.sin(theta)
+    wry = y - (ROBOTWIDTH / 2.0) * math.cos(theta)
     urx = u0 + k * wrx
     vrx = v0 - k * wry
     pygame.draw.circle(screen, blue, (int(urx), int(vrx)), int(k * WHEELBLOB))
@@ -324,14 +339,14 @@ while True:
     pygame.display.flip()
 
     # Actually now move robot based on chosen vL and vR
-    (x, y, theta) = setNewPosition(vL, vR, x, y, theta, dt)
+    (x, y, theta) = set_new_position(vL, vR, x, y, theta, dt)
 
-    moveBarriers(dt)
+    move_barriers(dt)
 
     # printBarriers()
 
     # Check collision
-    distToObstacle = calculateClosestObstacleDistance(x, y)
+    distToObstacle = calculate_closest_obstacle_distance(x, y)
     if distToObstacle < 0.001:
         print("Crash!")
         print("Result:", time.time() - startTime, "sec")
@@ -341,7 +356,7 @@ while True:
                     exit()
 
     # Check if robot has reached target
-    disttotarget = math.sqrt((x - barriers[targetindex][0])**2 + (y - barriers[targetindex][1])**2)
+    disttotarget = math.sqrt((x - barriers[targetindex][0]) ** 2 + (y - barriers[targetindex][1]) ** 2)
     if disttotarget < (BARRIERRADIUS + ROBOTRADIUS):
         print("Result:", time.time() - startTime, "sec")
         while True:
