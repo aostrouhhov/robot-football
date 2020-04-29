9d029b9a0e5929f7743e0a498d01a9d85a6d8172
stspbu <stardust.skg@gmail.com>
2020-04-29 22:00:01 +0300

    Merged fix for max_velo from master

--- utils.py
+++ utils.py
@@ -45,8 +45,8 @@ def move_to_dot(target_x, target_y, robot_x, robot_y, ball_x, ball_y, theta):
     if target_x == ball_x and target_y == ball_y:
         dist_to_target = math.sqrt((robot_x - target_x) ** 2 + (robot_y - target_y) ** 2)
         if dist_to_target < Robot.RADIUS + 1.5:
-            vl_chosen = vl_chosen + vl_chosen * 2
-            vr_chosen = vr_chosen + vr_chosen * 2
+            vl_chosen = vl_chosen + Robot.MAX_VELOCITY
+            vr_chosen = vr_chosen + Robot.MAX_VELOCITY
 
     if vl_chosen > Robot.MAX_VELOCITY or vr_chosen > Robot.MAX_VELOCITY:
         if vl_chosen > vr_chosen:
@@ -57,7 +57,7 @@ def move_to_dot(target_x, target_y, robot_x, robot_y, ball_x, ball_y, theta):
             diff = vl_chosen / vr_chosen
             vr_chosen = Robot.MAX_VELOCITY
             vl_chosen = vr_chosen * diff
-    else:
+        else:
             vl_chosen = Robot.MAX_VELOCITY
             vr_chosen = Robot.MAX_VELOCITY
 
