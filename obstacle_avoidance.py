c1c3a6a2e1c6e99f3a9968ef69b67fcce051fec3
KutuevVladimir <VladimirKutuev@gmail.com>
2020-05-15 20:12:54 +0300

    Changed some algorithm parameters

--- obstacle_avoidance.py
+++ obstacle_avoidance.py
@@ -12,12 +12,12 @@ def dump_obstacle_avoidance(robot_position, ball_predicted_positions, obstacles_
 m = 2
 n = 2
 
-alpha_p = 1.9  # F_att_1_coeff
+alpha_p = 2.3  # F_att_1_coeff
 
-alpha_v = 0.9  # F_att_2_coeff
+alpha_v = 1.3  # F_att_2_coeff
 
-F_rep_1_coeff = 1
-F_rep_2_coeff = 0.6
+F_rep_1_coeff = 0.5
+F_rep_2_coeff = 1.7
 
 glob_prev_p = None
 glob_prev_p_tar = None
@@ -26,8 +26,8 @@ glob_prev_obstacles = None
 ro_0 = 3 * UNITS_RADIUS
 eta = 2
 
-a_max = 0.3  # ROBOT_MAX_VELOCITY
-obstacle_distance_to_avoid = 2.0
+a_max = 1  # ROBOT_MAX_VELOCITY
+# obstacle_distance_to_avoid = 2.0
 
 def pfm_obstacle_avoidance(robot_position, ball_predicted_positions, obstacles_predicted_positions):
     global glob_prev_p
@@ -52,16 +52,19 @@ def pfm_obstacle_avoidance(robot_position, ball_predicted_positions, obstacles_p
     target_vec = F_att(p, p_tar, v, v_tar)
 
     for obstacle in obstacles_predicted_positions:
-        if numpy.linalg.norm(p - obstacle) < obstacle_distance_to_avoid:
-            prev_p_obs = min(prev_barriers, key=lambda bar: numpy.linalg.norm(obstacle - bar))
-            v_obs = obstacle - prev_p_obs
-            target_vec += F_rep(p, numpy.array([obstacle[0], obstacle[1]]), v, v_obs)
+        # if numpy.linalg.norm(p - obstacle) < obstacle_distance_to_avoid:
+        prev_p_obs = min(prev_barriers, key=lambda bar: numpy.linalg.norm(obstacle - bar))
+        v_obs = obstacle - prev_p_obs
+        target_vec += F_rep(p, numpy.array([obstacle[0], obstacle[1]]), v, v_obs)
 
     glob_prev_p = p
     glob_prev_p_tar = p_tar
     glob_prev_obstacles = obstacles_predicted_positions
 
     target_vec *= ROBOT_MAX_VELOCITY
+    target_vec_norm = numpy.linalg.norm(target_vec)
+    # if target_vec_norm > 2.9:
+    target_vec = 2.2 * target_vec / target_vec_norm
     return robot_position[0] + target_vec[0], robot_position[1] + target_vec[1]
 
 
@@ -73,16 +76,17 @@ def F_att(p, p_tar, v, v_tar):
     v_norm = numpy.linalg.norm(v_tar - v)
     p_scale = m * alpha_p * (numpy.linalg.norm(p_tar - p)) ** (m - 1)
     v_scale = n * alpha_v * v_norm ** (n - 1)
-    F_att_1 = p_scale * n_vec(p, p_tar)
+    n_rt = n_vec(p, p_tar)
+    F_att_1 = p_scale * n_rt
     # F_att_2 = v_scale * n_VRT(v, v_tar) if v_norm != 0 else 0
     if v_norm == 0:
         F_att_2 = 0
     else:
         n_vrt = n_vec(v, v_tar)
-        n_v = v / numpy.linalg.norm(v)
-        if v.dot(n_vrt) < 0:
-            x = n_v[0]
-            y = n_v[1]
+        # n_v = v / numpy.linalg.norm(v)
+        if n_rt.dot(n_vrt) < 0:
+            x = n_rt[0]
+            y = n_rt[1]
             a = n_vrt[0]
             b = n_vrt[1]
             new_n_vrt = numpy.array([a * (y ** 2 - x ** 2) - 2 * b * x * y, b * (x ** 2 - y ** 2) - 2 * a * x * y])
@@ -100,6 +104,8 @@ def F_rep(p, p_obs, v, v_obs):
         return numpy.array([0.0, 0.0])
     ro_m = v_ro ** 2 / a_max
     ro_s = numpy.linalg.norm(p_obs - p) - 2 * UNITS_RADIUS
+    if ro_s - ro_m >= ro_0:
+        return numpy.array([0.0, 0.0])
 
     F_rep_1 = F_rep_1_coeff * (-eta) / ((ro_s - ro_m) ** 2) * (1 + v_ro / a_max) * n_vec(p, p_obs)
     F_rep_2 = F_rep_2_coeff * eta * v_ro / (ro_s * a_max * (ro_s - ro_m) ** 2) * (v - v_obs - v_ro * n_vec(p, p_obs))
